be85b9239fd97baaa2c6add877a870bf78650fa3
unknown <hassanwebdeveloper8322@gmail.com>
2024-03-16 17:27:53 +0500

    fourth commit

--- db/connect.js
+++ db/connect.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const connectDB = () => {
   return (
     mongoose.set("strictQuery", false),
-    mongoose.connect(process.env.MONGO_URI)
+    mongoose.connect("mongodb+srv://Hassan:Hassan12345@nodejsexpressjs.yqtqanv.mongodb.net/SOCIAL-MEDIA-API?retryWrites=true&w=majority")
   )
 };
 
