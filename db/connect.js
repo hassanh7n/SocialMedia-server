245a6819c0c609d3bc2a1c13b868533ae42877b2
unknown <hassanwebdeveloper8322@gmail.com>
2024-03-16 16:54:04 +0500

    third commit

--- db/connect.js
+++ db/connect.js
@@ -1,20 +1,10 @@
-// const mongoose = require('mongoose')
-
-// const connectDB = (url) => {
-//   return mongoose.connect(url, {
-//     useNewUrlParser: true,
-//     useCreateIndex: true,
-//     useFindAndModify: false,
-//     useUnifiedTopology: true,
-//   })
-// }
-
-// module.exports = connectDB
 const mongoose = require('mongoose');
-mongoose.set('strictQuery', true);
 
 const connectDB = (url) => {
-  return mongoose.connect(url);
+  return (
+    mongoose.set("strictQuery", true),
+    mongoose.connect(url)
+  )
 };
 
 module.exports = connectDB;
