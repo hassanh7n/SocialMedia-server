fa8399a187b2757760c022b15c7dead0e01fad67
unknown <hassanwebdeveloper8322@gmail.com>
2024-03-16 16:56:55 +0500

    fourth commit

--- db/connect.js
+++ db/connect.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = (url) => {
   return (
-    mongoose.set("strictQuery", true),
+    mongoose.set("strictQuery", false),
     mongoose.connect(url)
   )
 };
